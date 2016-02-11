9ca6b8dc41bd7f88f1ae9d833831dfabea44a9bd
Christophe Pollet <cpollet@users.noreply.github.com>
2016-02-11 22:24:29 +0100

    format

--- read-api/service/src/main/java/net/cpollet/pocs/read/service/attributes/DynamicAttribute.java
+++ read-api/service/src/main/java/net/cpollet/pocs/read/service/attributes/DynamicAttribute.java
@@ -6,7 +6,7 @@ package net.cpollet.pocs.read.service.attributes;
 public class DynamicAttribute extends BaseAttribute {
     private final String method;
 
-    public DynamicAttribute( String name, String method) {
+    public DynamicAttribute(String name, String method) {
         super(name);
         this.method = method;
     }
