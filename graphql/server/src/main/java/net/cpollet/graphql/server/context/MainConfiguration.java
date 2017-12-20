0ba47a6c53c7c106c97089ec5eb33e4b0a775a67
Christophe Pollet <cpollet@users.noreply.github.com>
2017-12-21 00:41:31 +0100

    played a bit with dynamic graph definition

--- graphql/server/src/main/java/net/cpollet/graphql/server/context/MainConfiguration.java
+++ graphql/server/src/main/java/net/cpollet/graphql/server/context/MainConfiguration.java
@@ -1,17 +1,73 @@
 package net.cpollet.graphql.server.context;
 
 import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.Scalars;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLList;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.GraphQLTypeReference;
+import net.cpollet.graphql.server.graphql.PortfolioFetcher;
 import net.cpollet.graphql.server.graphql.Query;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Configuration
 public class MainConfiguration {
     @Bean
-    public SchemaParser graphqlSchema() {
+    @ConditionalOnProperty("staticSchema")
+    public GraphQLSchema staticGraphqlSchema() {
         return SchemaParser.newParser()
                 .file("graphql/schema.graphqls")
                 .resolvers(new Query())
-                .build();
+                .build()
+                .makeExecutableSchema();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(GraphQLSchema.class)
+    public GraphQLSchema dynamicGraphqlSchema() {
+        return GraphQLSchema.newSchema()
+                .query(GraphQLObjectType.newObject()
+                        .name("Query")
+                        .description("dynamic")
+                        .field(GraphQLFieldDefinition.newFieldDefinition()
+                                .name("allPortfolios")
+                                .type(GraphQLList.list(new GraphQLTypeReference("Portfolio")))
+                                .dataFetcher(new Query())
+                        )
+                        .build()
+                )
+                .additionalTypes(
+                        new HashSet<>(Arrays.asList(
+                                GraphQLObjectType.newObject()
+                                        .name("Portfolio")
+                                        .field(GraphQLFieldDefinition.newFieldDefinition()
+                                                .name("id")
+                                                .type(Scalars.GraphQLID)
+                                                //.dataFetcher(new PortfolioFetcher())
+                                                .build()
+                                        )
+                                        .field(GraphQLFieldDefinition.newFieldDefinition()
+                                                .name("type")
+                                                .type(Scalars.GraphQLString)
+                                                //.dataFetcher(new PortfolioFetcher())
+                                                .build()
+                                        )
+                                        .field(GraphQLFieldDefinition.newFieldDefinition()
+                                                .name("other")
+                                                .type(Scalars.GraphQLString)
+                                                //.dataFetcher(new PortfolioFetcher())
+                                                .build()
+                                        )
+                                        .build()
+                        )
+                        )
+                ).build();
     }
 }
--- graphql/server/src/main/java/net/cpollet/graphql/server/graphql/Query.java
+++ graphql/server/src/main/java/net/cpollet/graphql/server/graphql/Query.java
@@ -1,16 +1,32 @@
 package net.cpollet.graphql.server.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import graphql.TypeResolutionEnvironment;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.TypeResolver;
 import net.cpollet.graphql.server.domain.Portfolio;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
-public class Query implements GraphQLQueryResolver {
+public class Query implements GraphQLQueryResolver, DataFetcher<List<Portfolio>> {
     public List<Portfolio> getAllPortfolios() {
         return Arrays.asList(
                 new Portfolio("id-1", "trading"),
                 new Portfolio("id-2", "saving")
         );
+    }
+
+    @Override
+    public List<Portfolio> get(DataFetchingEnvironment environment) {
+        String uuid = UUID.randomUUID().toString();
+
+        environment.getFields().forEach(f -> System.out.println(uuid + " - fetching " + f.getName()));
+        environment.getFields().get(0).getSelectionSet().getSelections().forEach(s -> System.out.println(uuid + " -  >" + s));
+
+        return getAllPortfolios();
     }
 }
