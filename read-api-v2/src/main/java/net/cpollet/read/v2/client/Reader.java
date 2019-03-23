f2a8a6744e64cee163229cb2234ff9fdb6c98191
Christophe Pollet <cpollet@users.noreply.github.com>
2019-03-23 17:56:49 +0100

    Implemented DefaultAttributeStore, supporting automatic nesting

--- read-api-v2/src/main/java/net/cpollet/read/v2/client/Reader.java
+++ read-api-v2/src/main/java/net/cpollet/read/v2/client/Reader.java
@@ -1,47 +1,109 @@
 package net.cpollet.read.v2.client;
 
+import net.cpollet.read.v2.api.AttributeStore;
 import net.cpollet.read.v2.api.domain.Request;
-import net.cpollet.read.v2.api.domain.Response;
 import net.cpollet.read.v2.client.domain.PersonId;
 import net.cpollet.read.v2.client.domain.PortfolioId;
+import net.cpollet.read.v2.impl.AttributeDef;
+import net.cpollet.read.v2.impl.DefaultAttributeStore;
 import net.cpollet.read.v2.impl.ReadImpl;
+import net.cpollet.read.v2.impl.methods.FetchResult;
+import net.cpollet.read.v2.impl.methods.Method;
 import net.cpollet.read.v2.impl.methods.NestedMethod;
+import net.cpollet.read.v2.impl.methods.StandardMethod;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reader {
+    private static final Method<PortfolioId> standardPortfolio = new StandardMethod<>();
+    private static final Method<PersonId> standardPerson = new StandardMethod<>();
+
     public static void main(String[] args) {
-        PortfolioAttributeStore portfolioAttributeStore = new PortfolioAttributeStore();
-        PersonAttributeStore personAttributeStore = new PersonAttributeStore();
+        AttributeStore<PortfolioId> portfolioAttributeStore = new DefaultAttributeStore<>("portfolio");
+        AttributeStore<PersonId> personAttributeStore = new DefaultAttributeStore<>("person");
 
         ReadImpl<PortfolioId> portfolioReader = new ReadImpl<>(portfolioAttributeStore, new DefaultIdValidator<>());
         ReadImpl<PersonId> personReader = new ReadImpl<>(personAttributeStore, new DefaultIdValidator<>());
 
-        portfolioAttributeStore.nest(
-                Arrays.asList("owner.email", "owner.id_unknown", "owner.portfolio.id"),
-                new NestedMethod<>(
-                        "owner", portfolioAttributeStore.fetch("ownerId"), personReader, o -> new PersonId((Integer) o)
+        configurePortfolioAttributesStore(portfolioAttributeStore, personReader);
+        configurePersonAttributesStore(personAttributeStore, portfolioReader);
+
+        System.out.println(
+                portfolioReader.execute(
+                        new Request<>(
+                                Arrays.asList(
+                                        new PortfolioId("100000"),
+                                        new PortfolioId("200000"),
+                                        new PortfolioId("999999")
+                                ),
+                                Arrays.asList(
+                                        "id",
+                                        "status",
+                                        "ownerId",
+                                        "currency",
+                                        "unknown",
+                                        "owner.email",
+                                        "owner.unknown",
+                                        "owner.id_unknown",
+                                        "owner.portfolio.id"
+                                )
+                        )
                 )
         );
-        personAttributeStore.nest(
-                Collections.singletonList("portfolio.id"),
-                new NestedMethod<>(
-                        "portfolio", personAttributeStore.fetch("portfolioId"), portfolioReader, o -> new PortfolioId((String) o)
-                )
-        );
+    }
 
-        List<PortfolioId> ids = Arrays.asList(
-                new PortfolioId("100000"),
-                new PortfolioId("200000"),
-                new PortfolioId("999999")
-        );
-        List<String> attributes = Arrays.asList("status", "ownerId", "currency", "unknown", "owner.email", "owner.unknown", "owner.id_unknown", "owner.portfolio.id");
-        Request<PortfolioId> request = new Request<>(ids, attributes);
+    private static void configurePortfolioAttributesStore(AttributeStore<PortfolioId> portfolioAttributeStore, ReadImpl<PersonId> personReader) {
+        portfolioAttributeStore.add("id", new AttributeDef<>("id", standardPortfolio));
+        portfolioAttributeStore.add("status", new AttributeDef<>("status", standardPortfolio));
+        portfolioAttributeStore.add("currency", new AttributeDef<>("currency", standardPortfolio));
+        portfolioAttributeStore.add("ownerId", new AttributeDef<>("ownerId", new Method<PortfolioId>() {
+            @Override
+            public FetchResult<PortfolioId> fetch(List<AttributeDef<PortfolioId>> attributes, Collection<PortfolioId> ids) {
+                return new FetchResult<>(
+                        ids.stream()
+                                .collect(Collectors.toMap(
+                                        id -> id,
+                                        id -> attributes.stream()
+                                                .collect(Collectors.toMap(
+                                                        a -> a,
+                                                        a -> Integer.valueOf(id.get())
+                                                ))
+                                        )
 
-        Response<PortfolioId> response = portfolioReader.execute(request);
+                                )
+                );
+            }
+        }));
+        portfolioAttributeStore.nest(new NestedMethod<>(
+                "owner", portfolioAttributeStore.fetch("ownerId"), personReader, o -> new PersonId((Integer) o)
+        ));
+    }
 
-        System.out.println(response);
+    private static void configurePersonAttributesStore(AttributeStore<PersonId> personAttributeStore, ReadImpl<PortfolioId> portfolioReader) {
+        personAttributeStore.add("email", new AttributeDef<>("email", standardPerson));
+        personAttributeStore.add("portfolioId", new AttributeDef<>("portfolioId", new Method<PersonId>() {
+            @Override
+            public FetchResult<PersonId> fetch(List<AttributeDef<PersonId>> attributes, Collection<PersonId> ids) {
+                return new FetchResult<>(
+                        ids.stream()
+                                .collect(Collectors.toMap(
+                                        id -> id,
+                                        id -> attributes.stream()
+                                                .collect(Collectors.toMap(
+                                                        a -> a,
+                                                        a -> String.valueOf(id.get())
+                                                ))
+                                        )
+
+                                )
+                );
+            }
+        }));
+        personAttributeStore.nest(new NestedMethod<>(
+                "portfolio", personAttributeStore.fetch("portfolioId"), portfolioReader, o -> new PortfolioId((String) o)
+        ));
     }
 }
---
+++ read-api-v2/src/main/java/net/cpollet/read/v2/impl/DefaultAttributeStore.java
@@ -0,0 +1,65 @@
+package net.cpollet.read.v2.impl;
+
+import net.cpollet.read.v2.api.AttributeStore;
+import net.cpollet.read.v2.api.domain.Id;
+import net.cpollet.read.v2.impl.methods.NestedMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class DefaultAttributeStore<IdType extends Id> implements AttributeStore<IdType> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultAttributeStore.class);
+
+    private final String context;
+    private final Map<String, AttributeDef<IdType>> attributes;
+    private final Collection<NestedMethod<IdType, ? extends Id>> nestedAttributes;
+
+    public DefaultAttributeStore(String context) {
+        this.context = context;
+        attributes = new ConcurrentHashMap<>();
+        nestedAttributes = new Vector<>();
+    }
+
+    @Override
+    public void add(String name, AttributeDef<IdType> def) {
+        attributes.put(name, def);
+    }
+
+    @Override
+    public <NestedIdType extends Id> void nest(NestedMethod<IdType, NestedIdType> nestedMethod) {
+        nestedAttributes.add(nestedMethod);
+    }
+
+    @Override
+    public AttributeDef<IdType> fetch(String attributeName) {
+        if (attributes.containsKey(attributeName)) {
+            return attributes.get(attributeName);
+        }
+
+        return nestedAttributes.stream()
+                .filter(m -> m.supports(attributeName))
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        l -> {
+                            if (l.isEmpty()) {
+                                LOGGER.info("Attribute '{}:{}' does not exist", context, attributeName);
+                                return AttributeDef.invalid(attributeName);
+                            }
+
+                            if (l.size() > 1) {
+                                LOGGER.info("Attribute '{}:{}' is supported by multiple nested methods, expected only one", context, attributeName);
+                                return AttributeDef.invalid(attributeName);
+                            }
+
+                            AttributeDef<IdType> attributeDef = new AttributeDef<>(attributeName, l.get(0));
+                            attributes.put(attributeName, attributeDef);
+                            return attributeDef;
+                        }
+                ));
+    }
+}
