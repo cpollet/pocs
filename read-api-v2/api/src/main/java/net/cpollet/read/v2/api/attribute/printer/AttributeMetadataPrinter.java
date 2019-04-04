a2ca7cf0689284bb08e7fbfdf3593b25a6c16fbd
Christophe Pollet <cpollet@users.noreply.github.com>
2019-04-04 22:58:20 +0200

    Refactored AttributeMetadataPrinter to have more 'detailed' output

--- read-api-v2/api/src/main/java/net/cpollet/read/v2/api/attribute/printer/AttributeMetadataPrinter.java
+++ read-api-v2/api/src/main/java/net/cpollet/read/v2/api/attribute/printer/AttributeMetadataPrinter.java
@@ -5,41 +5,40 @@ import net.cpollet.read.v2.api.attribute.AttributeDef;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-public class AttributeMetadataPrinter implements AttributePrinter<Map<String, String>> {
-    private final Map<String, String> document;
+public class AttributeMetadataPrinter implements AttributePrinter<Map<String, Object>> {
+    private final Map<String, Object> document;
 
     public AttributeMetadataPrinter() {
         this.document = new HashMap<>();
     }
 
     @Override
-    public Map<String, String> print() {
+    public Map<String, Object> print() {
         return document;
     }
 
     @Override
-    public AttributePrinter<Map<String, String>> name(String name) {
+    public AttributePrinter<Map<String, Object>> name(String name) {
         document.put("name", name);
         return this;
     }
 
     @Override
-    public AttributePrinter<Map<String, String>> filtered(boolean filtered) {
-        document.put("filtered", String.valueOf(filtered));
+    public AttributePrinter<Map<String, Object>> filtered(boolean filtered) {
+        document.put("filtered", filtered);
         return this;
     }
 
     @Override
-    public AttributePrinter<Map<String, String>> deprecated(boolean deprecated) {
-        document.put("deprecated", String.valueOf(deprecated));
+    public AttributePrinter<Map<String, Object>> deprecated(boolean deprecated) {
+        document.put("deprecated", deprecated);
         return this;
     }
 
     @Override
-    public AttributePrinter<Map<String, String>> modes(Set<AttributeDef.Mode> modes) {
-        document.put("modes", modes.stream().map(Enum::toString).collect(Collectors.joining(",")));
+    public AttributePrinter<Map<String, Object>> modes(Set<AttributeDef.Mode> modes) {
+        document.put("modes", modes);
         return this;
     }
 }
