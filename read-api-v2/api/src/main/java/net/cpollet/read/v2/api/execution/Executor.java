d1da72cccb4299bfbed4395937e63fe2b1d68e9b
Christophe Pollet <cpollet@users.noreply.github.com>
2019-04-07 00:03:47 +0200

    Cleaned Executor

--- read-api-v2/api/src/main/java/net/cpollet/read/v2/api/execution/Executor.java
+++ read-api-v2/api/src/main/java/net/cpollet/read/v2/api/execution/Executor.java
@@ -8,13 +8,7 @@ public interface Executor<IdType extends Id> {
 
     Response<IdType> read(Request<IdType> request);
 
-    default Response<IdType> update(Request<IdType> request) {
-        throw new IllegalStateException();
-    }
-
-    default Response<IdType> reset(Request<IdType> request) {
-        throw new IllegalStateException();
-    }
+    Response<IdType> update(Request<IdType> request);
 
     Response<IdType> create(Request<IdType> request);
 
