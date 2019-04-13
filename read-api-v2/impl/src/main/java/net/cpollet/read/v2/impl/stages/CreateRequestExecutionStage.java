9831dc11cd2409d5314bf1e223383a88f5c65e2e
Christophe Pollet <cpollet@users.noreply.github.com>
2019-04-13 09:06:26 +0200

    Refactored CreateRequestExecutionStage

--- read-api-v2/impl/src/main/java/net/cpollet/read/v2/impl/stages/CreateRequestExecutionStage.java
+++ read-api-v2/impl/src/main/java/net/cpollet/read/v2/impl/stages/CreateRequestExecutionStage.java
@@ -10,9 +10,10 @@ import net.cpollet.read.v2.impl.attribute.AttributesGrouper;
 import net.cpollet.read.v2.impl.execution.InternalRequest;
 import net.cpollet.read.v2.impl.execution.InternalResponse;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,13 +23,15 @@ import java.util.stream.Collectors;
 public final class CreateRequestExecutionStage<T extends Id> implements Stage<T, AttributeDef<T>> {
     private final Stage<T, AttributeDef<T>> next;
     private final Stage<T, AttributeDef<T>> update;
-    private final AttributeDef<T> idAttribute;
+    private final Method<T> idAttributeMethod;
     private final Set<AttributeDef<T>> requiredAttributes;
 
     public CreateRequestExecutionStage(AttributeStore<T> store, Stage<T, AttributeDef<T>> update, Stage<T, AttributeDef<T>> next) {
         this.next = next;
         this.update = update;
-        this.idAttribute = store.idAttribute().orElse(null);
+        this.idAttributeMethod = store.idAttribute()
+                .map(AttributeDef::method)
+                .orElse(null);
         this.requiredAttributes = Collections.unmodifiableSet(
                 store.attributes().stream()
                         .filter(a -> a.modes().contains(AttributeDef.Mode.CREATE))
@@ -36,56 +39,133 @@ public final class CreateRequestExecutionStage<T extends Id> implements Stage<T,
         );
     }
 
-    // TODO deserves a refactoring...
     @Override
     public InternalResponse<T, AttributeDef<T>> execute(InternalRequest<T, AttributeDef<T>> request) {
-        if (idAttribute == null) {
-            return new InternalResponse<T, AttributeDef<T>>()
-                    .withErrors(Collections.singleton("creation not supported, no id-attribute provided"));
+        if (idAttributeMethod == null) {
+            throw new IllegalStateException("creation not supported, no id-attribute method provided");
         }
 
-        Set<AttributeDef<T>> missingAttributes = requiredAttributes.stream()
-                .filter(a -> !request.attributes().contains(a))
-                .collect(Collectors.toSet());
-
-        if (!missingAttributes.isEmpty()) {
-            return new InternalResponse<T, AttributeDef<T>>()
-                    .withErrors(
-                            missingAttributes.stream()
-                                    .map(a -> String.format("[%s] is required for creation", a))
-                                    .collect(Collectors.toList())
-                    );
+        Optional<InternalResponse<T, AttributeDef<T>>> errorResponse = validateRequiredAttributes(request.attributes());
+        if (errorResponse.isPresent()) {
+            return errorResponse.get();
         }
 
-        Map<Method<T>, List<AttributeDef<T>>> attributesGroupedByMethod = request.attributes(new AttributesGrouper<>());
+        List<AttributeDef<T>> mainAttributes = mainAttributes(request);
 
-        // we create the "main" entity instance with its attributes (i.e. the ones that are using the same method as the id attribute)
-        CreateResult<T> createResult = idAttribute.method().create(
-                request.principal(),
-                request.values(
-                        attributesGroupedByMethod.get(idAttribute.method())
-                )
-        );
-
-        if (!createResult.id().isPresent()) {
-            return new InternalResponse<T, AttributeDef<T>>()
-                    .withErrors(createResult.errors());
+        Result<T> idOrError = createMainEntity(request, mainAttributes);
+        if (!idOrError.isSuccessful()) {
+            return idOrError.response();
         }
-        T id = createResult.id().get();
 
-        // we continue with the remaining attributes, which actually becomes an update...
-        InternalResponse<T, AttributeDef<T>> updateResponse = update.execute(
-                request.withIds(Collections.singletonList(id))
-                        .withoutAttributes(attributesGroupedByMethod.get(idAttribute.method()))
+        InternalResponse<T, AttributeDef<T>> updateResponse = updateSecondaryEntities(
+                idOrError.id(),
+                request,
+                mainAttributes
         );
 
         return next
                 .execute(
                         request
-                                .withIds(Collections.singletonList(id))
+                                .withIds(Collections.singletonList(idOrError.id()))
                                 .addGuardedFlagIf(updateResponse.hasErrors(), Guarded.Flag.UPDATE_ERROR)
                 )
                 .mergeErrors(updateResponse)
                 .mergeMessages(updateResponse);
+    }
+
+    private Optional<InternalResponse<T, AttributeDef<T>>> validateRequiredAttributes(Collection<AttributeDef<T>> attributes) {
+        Set<AttributeDef<T>> missingAttributes = requiredAttributes.stream()
+                .filter(a -> !attributes.contains(a))
+                .collect(Collectors.toSet());
+
+        if (!missingAttributes.isEmpty()) {
+            return Optional.of(
+                    new InternalResponse<T, AttributeDef<T>>()
+                            .withErrors(
+                                    missingAttributes.stream()
+                                            .map(a -> String.format("[%s] is required for creation", a))
+                                            .collect(Collectors.toList())
+                            )
+            );
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Returns the list of all attributes that are handled by the same method as the id-attribute, i.e. the ones
+     * for which we don't want to execute an update afterwards.
+     */
+    private List<AttributeDef<T>> mainAttributes(InternalRequest<T, AttributeDef<T>> request) {
+        return request
+                .attributes(new AttributesGrouper<>())
+                .get(idAttributeMethod);
+    }
+
+    /**
+     * Create the "main" entity instance with its attributes (i.e. the ones that are using the same method as the id
+     * attribute)
+     */
+    private Result<T> createMainEntity(InternalRequest<T, AttributeDef<T>> request, List<AttributeDef<T>> attributes) {
+        CreateResult<T> createResult = idAttributeMethod.create(
+                request.principal(),
+                request.values(attributes)
+        );
+
+        return createResult.id()
+                .map(Result::success)
+                .orElseGet(() -> Result.error(
+                        new InternalResponse<T, AttributeDef<T>>()
+                                .withErrors(createResult.errors())
+                ));
+
+    }
+
+    /**
+     * Inserts the remaining attributes (i.e. the ones that are not using the same method as the id-attribute). This
+     * action actually becomes an update, as the main entity already exists.
+     */
+    private InternalResponse<T, AttributeDef<T>> updateSecondaryEntities(T id, InternalRequest<T, AttributeDef<T>> request, List<AttributeDef<T>> alreadyInsertedAttributes) {
+        return update.execute(
+                request
+                        .withIds(Collections.singletonList(id))
+                        .withoutAttributes(alreadyInsertedAttributes)
+        );
+    }
+
+    private static final class Result<T extends Id> {
+        private final T id;
+        private final InternalResponse<T, AttributeDef<T>> response;
+
+        Result(T id, InternalResponse<T, AttributeDef<T>> response) {
+            this.id = id;
+            this.response = response;
+        }
+
+        static <T extends Id> Result<T> success(T id) {
+            if (id == null) {
+                throw new NullPointerException("id cannot be null");
+            }
+            return new Result<>(id, null);
+        }
+
+        static <T extends Id> Result<T> error(InternalResponse<T, AttributeDef<T>> response) {
+            if (response == null) {
+                throw new NullPointerException("error cannot be null");
+            }
+            return new Result<>(null, response);
+        }
+
+        boolean isSuccessful() {
+            return id != null;
+        }
+
+        InternalResponse<T, AttributeDef<T>> response() {
+            return response;
+        }
+
+        public T id() {
+            return id;
+        }
     }
 }
